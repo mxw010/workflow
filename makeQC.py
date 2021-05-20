49b5624a8a7c89d1f184633e995ab42c146a355a
Meng <cherrywang1006@gmail.com>
2021-05-20 16:23:57 -0400

    initial commit

---
+++ makeQC.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import os, glob
+import pickle
+import json
+import re
+
+
+design_sheet = pd.read_csv('Design_sheet.csv', header=0, index_col=0)
+primary = pd.read_csv('PrimaryQC.csv', header=0,index_col=0)
+spikeIn = pd.read_csv('SpikeInQC.csv', header=0,index_col=0)
+
+basedir = '/home/gdstantonlab/mxw010/Data/Ben/pc-chip-seq/GSL-BS-2005'
+
+uni_input = primary[primary['Target'] == "INPUT"]['Library ID']
+matching_sample = [ design_sheet[design_sheet['Input'] == x].iloc[0]['Sample'] for x in uni_input ]
+input_data = pd.DataFrame({'Sample':matching_sample, 'Input':uni_input})
+
+primary['total_reads'] = spikeIn['total_reads'] =''
+primary['mapped'] = spikeIn['mapped'] =''
+primary['proper_mapped'] = spikeIn['proper_mapped'] = ''
+primary['filtered'] = spikeIn['filtered'] = ''
+primary['deduped'] = spikeIn['deduped'] = ''
+
+for i in range(0,len(primary)):
+	library = primary.iloc[i]['Library ID']
+	target = primary.iloc[i]['Target']
+	cell = primary.iloc[i]['Primary Cell Type']
+	if (target == 'INPUT'):
+		continue
+	primary_qc_path = glob.glob(basedir + "/" + target + "/" + cell + "*" + "/**/" + "align_primary" + "/**/" + 'call-qc_report/execution/qc.json', recursive=True)
+	spikeIn_qc_path = glob.glob(basedir + "/" + target + "/" + cell + "*" + "/**/" + "spike_in" + "/**/" + 'call-qc_report/execution/qc.json', recursive=True)
+	with open(primary_qc_path[0]) as f:
+		primary_qc = json.load(f)
+	with open(spikeIn_qc_path[0]) as f:
+		spikeIn_qc = json.load(f)
+	primary.loc[primary.index[i], 'total_reads'] = primary_qc['align']['samstat']['rep1']['total_reads']
+	primary.loc[primary.index[i], 'mapped'] = primary_qc['align']['samstat']['rep1']['mapped_reads']
+	primary.loc[primary.index[i], 'proper_mapped'] = primary_qc['align']['samstat']['rep1']['properly_paired_reads']
+	primary.loc[primary.index[i], 'filtered'] = primary_qc['align']['dup']['rep1']['paired_reads']*2
+	primary.loc[primary.index[i], 'deduped'] = primary_qc['align']['nodup_samstat']['rep1']['total_reads']
+	spikeIn.loc[spikeIn.index[i], 'total_reads'] = spikeIn_qc['align']['samstat']['rep1']['total_reads']
+	spikeIn.loc[spikeIn.index[i], 'mapped'] = spikeIn_qc['align']['samstat']['rep1']['mapped_reads']
+	spikeIn.loc[spikeIn.index[i], 'proper_mapped'] = spikeIn_qc['align']['samstat']['rep1']['properly_paired_reads']
+	spikeIn.loc[spikeIn.index[i], 'filtered'] = spikeIn_qc['align']['dup']['rep1']['paired_reads']*2
+	spikeIn.loc[spikeIn.index[i], 'deduped'] = spikeIn_qc['align']['nodup_samstat']['rep1']['total_reads']
+	if any(re.search(library, x,re.IGNORECASE) for x in input_data['Sample']):
+		input=input_data[input_data['Sample'] == library]['Input'].to_string(index=False).strip()
+		orig_indx = [ key for key, val in enumerate(primary['Library ID']) if val == input ][0]
+		primary.loc[primary.index[orig_indx], 'total_reads'] = primary_qc['align']['samstat']['ctl1']['total_reads']
+		primary.loc[primary.index[orig_indx], 'mapped'] = primary_qc['align']['samstat']['ctl1']['mapped_reads']
+		primary.loc[primary.index[orig_indx], 'proper_mapped'] = primary_qc['align']['samstat']['ctl1']['properly_paired_reads']
+		primary.loc[primary.index[orig_indx], 'filtered'] = primary_qc['align']['dup']['ctl1']['paired_reads']*2
+		primary.loc[primary.index[orig_indx], 'deduped'] = primary_qc['align']['nodup_samstat']['ctl1']['total_reads']
+		spikeIn.loc[spikeIn.index[orig_indx], 'total_reads'] = spikeIn_qc['align']['samstat']['ctl1']['total_reads']
+		spikeIn.loc[spikeIn.index[orig_indx], 'mapped'] = spikeIn_qc['align']['samstat']['ctl1']['mapped_reads']
+		spikeIn.loc[spikeIn.index[orig_indx], 'proper_mapped'] = spikeIn_qc['align']['samstat']['ctl1']['properly_paired_reads']
+		spikeIn.loc[spikeIn.index[orig_indx], 'filtered'] = spikeIn_qc['align']['dup']['ctl1']['paired_reads']*2
+		spikeIn.loc[spikeIn.index[orig_indx], 'deduped'] = spikeIn_qc['align']['nodup_samstat']['ctl1']['total_reads']
+primary.to_csv("primary_alignment_stats.csv", index=False)
+spikeIn.to_csv("SpikeIn_alignment_stats.csv", index=False)
+
+
